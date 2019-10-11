2e7e6ccccf97970ed7d105100e4585c5548bfae9
Gillian McMahon <54951202+heygillianm@users.noreply.github.com>
2019-10-10 22:09:37 -0700

    Update

--- WinPercentage.java
+++ WinPercentage.java
@@ -5,13 +5,15 @@ public class WinPercentage
 {
   public static void main (String [] args) {
     Scanner scan = new Scanner(System.in);
+    DecimalFormat dec = new DecimalFormat(".##");
+    
 
     System.out.println("Enter number of games played:");
     int played = scan.nextInt();
     System.out.println("Enter number of games won:");
     int won = scan.nextInt();
 
-    while (int == played && int == won)
+    while (played > 0 && won < played)
     {
       double finalWin = ((won/played) * 100);
       System.out.println("Your winning percentage is: " + finalWin);
